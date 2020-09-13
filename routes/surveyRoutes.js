590425b6e082b677ba80d23543636ffcd967f60f
JIang-zzz <z1335447535@gmail.com>
2020-09-13 23:30:30 +1000

    added keys and fixed import

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const requireLogin = require("../middlewares");
+const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
 const Survey = mongoose.model("surveys");
 
