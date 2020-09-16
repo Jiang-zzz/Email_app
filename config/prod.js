3789aac72ffdc10bff20f649d60bf1c514a55a51
JIang-zzz <z1335447535@gmail.com>
2020-09-16 23:25:05 +1000

    added redirect page

--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,12 @@
+const { redirectDomain } = require("./dev");
+
 module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
-  stripePublishableKey:process.env.STRIPE_PUBLISHABLE_KEY,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
-  sendGridKey: process.env.SEND_GRID_KEY
+  sendGridKey: process.env.SEND_GRID_KEY,
+  redirectDomain: process.env.REDIRECT_DOMAIN,
 };
