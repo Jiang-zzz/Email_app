90df4f69d39b419afb7e28752624360519aea8b2
JIang-zzz <z1335447535@gmail.com>
2020-09-25 18:49:22 +1000

    adjust button position

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -5,7 +5,7 @@ const Dashboard = () => {
   return (
     <div>
       <SurveyList />
-      <div className="fixed-action-btn">
+      <div className="fixed-action-btn" style={{right:"100px"}}>
         <Link to="/surveys/new" className="btn-floating btn-large red">
           <i className="large material-icons">add</i>
         </Link>
